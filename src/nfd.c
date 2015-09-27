2b87971ad3ee92fed8a67e2ed215befda8a45156
Dimitris Alikaniotis <dim.alikaniotis@gmail.com>
2015-09-27 17:36:45 +0100

    bugfixing

--- src/nfd.c
+++ src/nfd.c
@@ -17,34 +17,27 @@
 #include <stdio.h>
 #include <R.h>
 
-#define MAX(x, y) x>=y ? x : y
 #define MIN(x, y) x<=y ? x : y
 
 void nfd(int *v1, int *v2, int *v1s, int *v2s, int *vec, double *ans) {
-  int a, sumvec=0;
-  int min = MIN(*v1s, *v2s);
-  int max = MAX(*v2s, *v2s);
-  int sum = 0;
+  int a, min = MIN(*v1s, *v2s);
+  int tmp, sum = 0, sumvec = 0;
   
   for (a=0; a<min; a++) {
-    vec[a] = abs(v1[a] - v2[a]);
+    sumvec += (vec[a] = abs(v1[a] - v2[a]));
     sum += (v1[a] + v2[a]);
-    sumvec += sum;
   }
   
-  if (*v1s > *v2s) {
+  if (*v1s > *v2s)
     for (; a<*v1s; a++) {
-      vec[a] = v1[a];
-      sum += v1[a];
-      sumvec += vec[a];
+      sum += (tmp = vec[a] = v1[a]);
+      sumvec += tmp;
     }
-  } else {
+  else
     for (; a<*v2s; a++) {
-      vec[a] = v2[a];
-      sum += v2[a];
-      sumvec += vec[a];
+      sum += (tmp = vec[a] = v2[a]);
+      sumvec += tmp;
     }
-  }
 
   *ans = (double) sumvec / sum;
 }
