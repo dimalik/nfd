3b1087d5751098741475d8423ae2a79e5e11d475
Dimitris Alikaniotis <dim.alikaniotis@gmail.com>
2015-09-27 15:57:39 +0100

    simplified c
    got rid of multiple for loops

--- src/nfd.c
+++ src/nfd.c
@@ -18,34 +18,33 @@
 #include <R.h>
 
 #define MAX(x, y) x>=y ? x : y
+#define MIN(x, y) x<=y ? x : y
 
 void nfd(int *v1, int *v2, int *v1s, int *v2s, int *vec, double *ans) {
-  int a, sum1=0, sum2=0, sumvec=0;
-  int t = MAX(*v1s, *v2s);
+  int a, sumvec=0;
+  int min = MIN(*v1s, *v2s);
+  int max = MAX(*v2s, *v2s);
+  int sum = 0;
   
-  if (*v1s >= *v2s) {
-    for (a=0; a<*v2s; a++) {
-      vec[a] = abs(v1[a] - v2[a]);
-      sum1 += v1[a];
-      sum2 += v2[a];
-    }
-    if (*v1s > *v2s)
-      for (; a<*v1s; a++) {
-        vec[a] = v1[a];
-        sum1 += v1[a];
-      }
-  } else {
-    for (a=0; a<*v1s; a++) {
-      vec[a] = abs(v1[a] - v2[a]);
-      sum1 += v1[a];
-      sum2 += v2[a];
-    }
-    for (; a<*v2s; a++) {
-      vec[a] = v2[a];
-      sum2 += v2[a];
-    }
+  for (a=0; a<min; a++) {
+    vec[a] = abs(v1[a] - v2[a]);
+    sum += (v1[a] + v2[a]);
+    sumvec += sum;
   }
   
-  for (a=0; a<t; a++) sumvec += vec[a];
-  *ans = (double) sumvec / (sum1 + sum2);
+  if (*v1s > *v2s) {
+    for (; a<*v1s; a++) {
+      vec[a] = v1[a];
+      sum += v1[a];
+      sumvec += vec[a];
+    }
+  } else {
+    for (; a<*v2s; a++) {
+      vec[a] = v2[a];
+      sum += v2[a];
+      sumvec += vec[a];
+    }
+  }
+
+  *ans = (double) sumvec / sum;
 }
